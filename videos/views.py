d9bf72a0c8c828e84f7b7fa2e3d194cd630117ea
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2025-11-16 14:11:20 +0100

    listage des vidéos ajouté depuis le coté admin

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,10 @@
+from django.utils import timezone
+from django.shortcuts import render
+from .models import Video
 from django.shortcuts import render
 
+
 # Create your views here.
+def video_list(request):
+    videos = Video.objects.filter(uploaded_at__lte=timezone.now()).order_by('uploaded_at')
+    return render(request, 'videos/video_list.html', {'videos': videos})

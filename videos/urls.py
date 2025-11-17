0d0ddaa1c6312f36c1fe9bfeaf09f595fe74760e
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2025-11-17 07:44:24 +0100

    Ajout, modification, détails vidéo.

--- videos/urls.py
+++ videos/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.video_list, name='video_list')
+    path('', views.video_list, name='video_list'),
+    path('video/<int:pk>/', views.video_detail, name='video_detail'),
+    path('video/new/', views.video_new, name='video_new'),
+    path('video/<int:pk>/edit/', views.video_edit, name='video_edit'),
 ]
--- videos/views.py
+++ videos/views.py
@@ -1,10 +1,43 @@
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Video
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .forms import VideoForm
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def video_list(request):
     videos = Video.objects.filter(uploaded_at__lte=timezone.now()).order_by('uploaded_at')
     return render(request, 'videos/video_list.html', {'videos': videos})
+
+def video_detail(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    return render(request, 'videos/video_detail.html', {'video': video})
+
+def video_new(request):
+    if request.method == "POST":
+        form = VideoForm(request.POST)
+        if form.is_valid():
+            video = form.save(commit=False)
+            video.author = request.user
+            video.published_date = timezone.now()
+            video.save()
+            return redirect('video_detail', pk=video.pk)
+    else:
+        form = VideoForm()
+    return render(request, 'videos/video_edit.html', {'form': form})
+
+def video_edit(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    if request.method == "POST":
+        form = VideoForm(request.POST, instance=video)
+        if form.is_valid():
+            video = form.save(commit=False)
+            video.author = request.user
+            video.published_date = timezone.now()
+            video.save()
+            return redirect('video_detail', pk=video.pk)
+    else:
+        form = VideoForm(instance=video)
+    return render(request, 'videos/video_edit.html', {'form': form})
